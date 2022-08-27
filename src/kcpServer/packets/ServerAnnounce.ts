c8ca194ce6453b7d9e2d19772b6c2454fe0ee000
NotArandomGUY <hhin0001@gmail.com>
2022-08-27 20:59:03 +0800

    Add ServerAnnounce packet

---
+++ src/kcpServer/packets/ServerAnnounce.ts
@@ -0,0 +1,25 @@
+import Packet, { PacketContext, PacketInterface } from '#/packet'
+import { AnnounceData } from '@/types/proto'
+
+export interface ServerAnnounceNotify {
+  announceDataList: AnnounceData[]
+}
+
+class ServerAnnouncePacket extends Packet implements PacketInterface {
+  constructor() {
+    super('ServerAnnounce')
+  }
+
+  async sendNotify(context: PacketContext, announceDataList: AnnounceData[]): Promise<void> {
+    const notifyData: ServerAnnounceNotify = { announceDataList }
+
+    await super.sendNotify(context, notifyData)
+  }
+
+  async broadcastNotify(contextList: PacketContext[], announceDataList: AnnounceData[]): Promise<void> {
+    await super.broadcastNotify(contextList, announceDataList)
+  }
+}
+
+let packet: ServerAnnouncePacket
+export default (() => packet = packet || new ServerAnnouncePacket())()
---
+++ src/types/proto/AnnounceData.ts
@@ -0,0 +1,11 @@
+export interface AnnounceData {
+  countDownText?: string
+  centerSystemText?: string
+  countDownFrequency?: number
+  configId?: number
+  beginTime?: number
+  centerSystemFrequency?: number
+  dungeonConfirmText?: string
+  isCenterSystemLast5EveryMinutes?: boolean
+  endTime?: number
+}
