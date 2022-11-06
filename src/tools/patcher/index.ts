51762a8ef6f093bb2b84e1efa138c8b661756b82
Kawa <lxr2061895177@163.com>
2022-11-06 16:58:38 +0800

    Optimize gpatch

--- src/tools/patcher/index.ts
+++ src/tools/patcher/index.ts
@@ -2,7 +2,7 @@ import config from '@/config'
 import translate from '@/translate'
 import TError from '@/translate/terror'
 import { deleteFile, dirExists, fileExists, fileSize, readFile, writeFile } from '@/utils/fileSystem'
-import { versionStrToNum } from '@/utils/version'
+import {versionStrToL, versionStrToNum} from '@/utils/version'
 import { join } from 'path'
 import { patchMetadata } from '../metadata'
 import { UAPatch } from '../UAPatch'
@@ -23,61 +23,57 @@ async function getDataDir(gameDir: string): Promise<string> {
 export async function patchGame(gameDir: string) {
   const dataDir = await getDataDir(gameDir)
   if (dataDir == null) throw new TError('message.tools.patcher.error.noDataDir')
+  const vL:Number[] = versionStrToL(config.version) //get version
 
-  try {
+  try { // Default
     console.log(translate('message.tools.patcher.info.patchMeta'))
-
     await patchMetadata(join(dataDir, nativeMetadataPath), join(dataDir, managedMetadataPath))
   } catch (err) {
-    console.log(err)
+    console.log(err) // TODO: add "please Elevate privileges" in json
   }
+  try { //version > 3.0.0
+    if (vL[0] > 3 || (vL[0] == 3 && vL[1] != 0 && vL[2] != 0)) {
+      console.log(translate('message.tools.patcher.info.patchUA'))
+      const UApath = join(dataDir, uaPath)
+      const bakUApath = join(dataDir, `${uaPath}.bak`)
 
-  if (versionStrToNum(config.version) < 0x030032) return
+      if (!await fileExists(UApath)) throw new TError('generic.fileNotFound', UApath)
 
-  try {
-    console.log(translate('message.tools.patcher.info.patchUA'))
+      // Check if backup already exists
+      if (await fileExists(bakUApath) && await fileSize(UApath) === await fileSize(bakUApath)) throw new TError('message.tools.patcher.error.patched')
 
-    const UApath = join(dataDir, uaPath)
-    const bakUApath = join(dataDir, `${uaPath}.bak`)
-
-    if (!await fileExists(UApath)) throw new TError('generic.fileNotFound', UApath)
-
-    // Check if backup already exists
-    if (await fileExists(bakUApath) && await fileSize(UApath) === await fileSize(bakUApath)) throw new TError('message.tools.patcher.error.patched')
-
-    // Create backup
-    await writeFile(bakUApath, await readFile(UApath))
-
-    await UAPatch(bakUApath, UApath)
+      // Create backup
+      await writeFile(bakUApath, await readFile(UApath))
+      await UAPatch(bakUApath, UApath)
+    }
   } catch (err) {
-    console.log(err)
+    console.log(err) // TODO: add "please Elevate privileges" in json
   }
 }
 
 export async function unpatchGame(gameDir: string) {
   const dataDir = await getDataDir(gameDir)
   if (dataDir == null) throw new TError('message.tools.patcher.error.noDataDir')
+  const vL:Number[] = versionStrToL(config.version) //get version
 
-  try {
+  try { // Default
     console.log(translate('message.tools.patcher.info.unpatchMeta'))
-
     await writeFile(join(dataDir, managedMetadataPath), await readFile(join(dataDir, nativeMetadataPath)))
   } catch (err) {
     console.log(err)
   }
+  try { //version > 3.0.0
+    if (vL[0] > 3 || (vL[0] == 3 && vL[1] != 0 && vL[2] != 0)) {
+      console.log(translate('message.tools.patcher.info.unpatchUA'))
 
-  if (versionStrToNum(config.version) < 0x030032) return
+      const UApath = join(dataDir, uaPath)
+      const bakUApath = join(dataDir, `${uaPath}.bak`)
 
-  try {
-    console.log(translate('message.tools.patcher.info.unpatchUA'))
+      if (!await fileExists(bakUApath)) throw new TError('generic.fileNotFound', bakUApath)
 
-    const UApath = join(dataDir, uaPath)
-    const bakUApath = join(dataDir, `${uaPath}.bak`)
-
-    if (!await fileExists(bakUApath)) throw new TError('generic.fileNotFound', bakUApath)
-
-    await writeFile(UApath, await readFile(bakUApath))
-    await deleteFile(bakUApath)
+      await writeFile(UApath, await readFile(bakUApath))
+      await deleteFile(bakUApath)
+    }
   } catch (err) {
     console.log(err)
   }
