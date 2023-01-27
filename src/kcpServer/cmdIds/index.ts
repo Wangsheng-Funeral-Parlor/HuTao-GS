d657c8dfd73ff3a3c5e57655fe9ee5d593a14a72
Kıvanç Çelik <104195873+crowity@users.noreply.github.com>
2023-01-27 14:31:59 +0300

    Added 3.4.0

--- src/kcpServer/cmdIds/index.ts
+++ src/kcpServer/cmdIds/index.ts
@@ -7,6 +7,7 @@ import ver2_7 from './ver2_7'
 import ver2_8 from './ver2_8'
 import ver2_8_50 from './ver2_8_50'
 import ver3_3_0 from './ver3_3_0'
+import ver3_4_0 from './ver3_4_0'
 
 const versionMap: { [version: string]: CmdIds } = {
   '1.4.50': ver1_4_50,
@@ -35,7 +36,8 @@ const versionMap: { [version: string]: CmdIds } = {
   '3.2.51': ver3_3_0,
   '3.2.52': ver3_3_0,
   '3.2.53': ver3_3_0,
-  '3.3.0': ver3_3_0
+  '3.3.0': ver3_3_0,
+  '3.4.0': ver3_4_0
 }
 
 export const cmdIds: CmdIds = versionMap[config.version] || versionMap[DEFAULT_CONFIG.version] || {}
