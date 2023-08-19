7d794dceea874e8b02d44e8bf6389c0edf1b9203
NotArandomGUY <hhin0001@gmail.com>
2023-08-19 15:31:19 +0800

    Update log meesage

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,19 @@
+import AbilityData from '$/gameData/data/AbilityData'
+import AvatarData from '$/gameData/data/AvatarData'
+import DungeonData from '$/gameData/data/DungeonData'
+import GadgetData from '$/gameData/data/GadgetData'
+import GrowCurveData from '$/gameData/data/GrowCurveData'
+import MapAreaData from '$/gameData/data/MapAreaData'
+import MaterialData from '$/gameData/data/MaterialData'
+import MonsterData from '$/gameData/data/MonsterData'
+import ReliquaryData from '$/gameData/data/ReliquaryData'
+import SceneData from '$/gameData/data/SceneData'
+import ShopData from '$/gameData/data/ShopData'
+import SkillData from '$/gameData/data/SkillData'
+import TalentData from '$/gameData/data/TalentData'
+import WeaponData from '$/gameData/data/WeaponData'
+import WeatherData from '$/gameData/data/WeatherData'
+import WorldData from '$/gameData/data/WorldData'
 import GlobalState from '@/globalState'
 import KcpServer from '@/kcpServer'
 import { waitMs } from '@/utils/asyncWait'
@@ -18,22 +34,6 @@ import Update from './update'
 import Authenticator from './utils/authenticator'
 import { detachedSpawn, execCommand } from './utils/childProcess'
 import { dirExists } from './utils/fileSystem'
-import AbilityData from '$/gameData/data/AbilityData'
-import DungeonData from '$/gameData/data/DungeonData'
-import GadgetData from '$/gameData/data/GadgetData'
-import GrowCurveData from '$/gameData/data/GrowCurveData'
-import MonsterData from '$/gameData/data/MonsterData'
-import ReliquaryData from '$/gameData/data/ReliquaryData'
-import SkillData from '$/gameData/data/SkillData'
-import TalentData from '$/gameData/data/TalentData'
-import WeaponData from '$/gameData/data/WeaponData'
-import MapAreaData from '$/gameData/data/MapAreaData'
-import WeatherData from '$/gameData/data/WeatherData'
-import AvatarData from '$/gameData/data/AvatarData'
-import WorldData from '$/gameData/data/WorldData'
-import MaterialData from '$/gameData/data/MaterialData'
-import SceneData from '$/gameData/data/SceneData'
-import ShopData from '$/gameData/data/ShopData'
 
 const {
   serverName,
@@ -336,58 +336,41 @@ export default class Server {
   }
 
   async resourceCache() {
-    const tLogger = new TLogger()
-
-    tLogger.info("message.cache.info.start")
+    logger.info('message.cache.info.start')
 
     await AbilityData.getData()
-    tLogger.debug("message.cache.debug.ability")
-  
+    logger.debug('message.cache.debug.ability')
     await AvatarData.getData()
-    tLogger.debug("message.cache.debug.avatar")
-  
+    logger.debug('message.cache.debug.avatar')
     await DungeonData.getData()
-    tLogger.debug("message.cache.debug.dungeon")
-  
+    logger.debug('message.cache.debug.dungeon')
     await GadgetData.getData()
-    tLogger.debug("message.cache.debug.gadget")
-  
+    logger.debug('message.cache.debug.gadget')
     await GrowCurveData.getData()
-    tLogger.debug("message.cache.debug.growCurve")
-  
+    logger.debug('message.cache.debug.growCurve')
     await MapAreaData.getData()
-    tLogger.debug("message.cache.debug.mapArea")
-  
+    logger.debug('message.cache.debug.mapArea')
     await MaterialData.getData()
-    tLogger.debug("message.cache.debug.material")
-  
+    logger.debug('message.cache.debug.material')
     await MonsterData.getData()
-    tLogger.debug("message.cache.debug.monster")
-  
+    logger.debug('message.cache.debug.monster')
     await ReliquaryData.getData()
-    tLogger.debug("message.cache.debug.reliquary")
-  
+    logger.debug('message.cache.debug.reliquary')
     await SceneData.getData()
-    tLogger.debug("message.cache.debug.scene")
-  
+    logger.debug('message.cache.debug.scene')
     await ShopData.getData()
-    tLogger.debug("message.cache.debug.shop")
-  
+    logger.debug('message.cache.debug.shop')
     await SkillData.getData()
-    tLogger.debug("message.cache.debug.skill")
-  
+    logger.debug('message.cache.debug.skill')
     await TalentData.getData()
-    tLogger.debug("message.cache.debug.talent")
-  
+    logger.debug('message.cache.debug.talent')
     await WeaponData.getData()
-    tLogger.debug("message.cache.debug.weapon")
-  
+    logger.debug('message.cache.debug.weapon')
     await WeatherData.getData()
-    tLogger.debug("message.cache.debug.weather")
-  
+    logger.debug('message.cache.debug.weather')
     await WorldData.getData()
-    tLogger.debug("message.cache.debug.world")
-  
-    tLogger.info("message.cache.info.success")
+    logger.debug('message.cache.debug.world')
+
+    logger.info('message.cache.info.success')
   }
 }
